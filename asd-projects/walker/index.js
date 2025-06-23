ee5bcef8dfab0a334849131c24040b481b4efd51
JessicaGH1 <jessicagaliciahernandez985@gmail.com>
2025-06-23 14:58:44 +0000

    finished walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -18,13 +18,13 @@ function runProgram(){
   }
 
   var walker2 = {
-    positionX2: 550,
+    positionX2: 500,
     speedX2: 0,
-    positionY2: 550,
+    positionY2: 500,
     speedY2: 0
   }
 
-  // Game Item Objects
+  // Game Item Objects for 1st and 2nd box
   var KEY = {
     LEFT: 37,
     UP: 38,
@@ -53,6 +53,7 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
 
+  // if boxes clicked on, they turn the same color
   var randomColor = "#000000".replace(/0/g, function () {
   return (~~(Math.random() * 16)).toString(16); 
   });
@@ -68,6 +69,7 @@ function runProgram(){
     $('#walker2').css('background-color', randomColor);
   }
 
+  // the game 
   function newFrame() {
     repositionGameItem();
     repositionGameItem2();
@@ -83,8 +85,6 @@ function runProgram(){
   /* 
   Called in response to events.
   */
-
-  
   function handleKeyDown(event) {
    if (event.which === KEY.LEFT){
     walker.speedX = -5;
@@ -105,21 +105,7 @@ function runProgram(){
    }
   }
   
-
-  /*
-  function handleKeyDown(event) {
-   if (event.which === KEY2.A){
-    walker2.speedX2 = -5;
-   } else if (event.which === KEY2.W){
-    walker2.speedY2 = -5;
-   } else if (event.which === KEY2.D){
-    walker2.speedX2 = 5;
-   } else if (event.which === KEY2.S){
-    walker2.speedY2 = 5;
-   }
-  }
-*/
-  
+  // function that stops box when user no longer pressing down on keys
   function handleKeyUp(event){
     walker.speedX = 0;
     walker.speedY = 0;
@@ -148,26 +134,26 @@ function runProgram(){
 
   
   function repositionGameItem2(){
-    walker2.positionX2 += walker2.speedX2; // update the position of the box along the x-axis
-    walker2.positionY2 += walker2.speedY2; // update the position of the box along the x-axis
+    walker2.positionX2 += walker2.speedX2; // update the position of the 1st box along the x-axis
+    walker2.positionY2 += walker2.speedY2; // update the position of the 2nd box along the x-axis
   }
 
 
   function redrawGameItem(){
-    $("#walker").css("left", walker.positionX); // draw the box in the new location, positionX pixels away from the "left"
-    $("#walker").css("top", walker.positionY); // draw the box in the new location, positionX pixels away from the "left"
+    $("#walker").css("left", walker.positionX); // draw the 1st box in the new location, positionX pixels away from the "left"
+    $("#walker").css("top", walker.positionY); // draw the 1st box in the new location, positionY pixels away from the "top"
   }
 
 
   function redrawGameItem2(){
-    $("#walker2").css("left", walker2.positionX2); // draw the second box in the new location, positionX pixels away from the "left"
-    $("#walker2").css("top", walker2.positionY2); // draw the second box in the new location, positionX pixels away from the "left"
+    $("#walker2").css("left", walker2.positionX2); // draw the 2nd box in the new location, positionX2 pixels away from the "left"
+    $("#walker2").css("top", walker2.positionY2); // draw the 2nd box in the new location, positionY2 pixels away from the "top"
   }
 
   function wallCollision(){
-    if (walker.positionX > $("#board").width()-50){ // right wall
+    if (walker.positionX > $("#board").width()-100){ // right wall
       walker.positionX -= walker.speedX
-    } else if (walker.positionY > $("#board").height()-50){ // bottom
+    } else if (walker.positionY > $("#board").height()-100){ // bottom wall
       walker.positionY -= walker.speedY
     } else if (walker.positionX < 0){ // left wall
       walker.positionX -= walker.speedX
@@ -177,9 +163,9 @@ function runProgram(){
   }
 
   function wallCollision2(){
-    if (walker2.positionX2 > $("#board").width()-50){ // right wall
+    if (walker2.positionX2 > $("#board").width()-100){ // right wall
       walker2.positionX2 -= walker2.speedX2
-    } else if (walker2.positionY2 > $("#board").height()-50){ // bottom
+    } else if (walker2.positionY2 > $("#board").height()-100){ // bottom wall
       walker2.positionY2 -= walker2.speedY2
     } else if (walker2.positionX2 < 0){ // left wall
       walker2.positionX2 -= walker2.speedX2
@@ -188,13 +174,15 @@ function runProgram(){
     }
   }
 
+  // checks if 1st and 2nd box are in the same position
+  // 1st box is 'it' so that box turns dark red
   function playersCollide(){
     if (walker.positionX === walker2.positionX2 && walker.positionY === walker2.positionY2){
       $('#walker').css('background-color', 'darkred');
       walker.positionX = 0;
       walker.positionY = 0;
-      walker2.positionX2 = 550;
-      walker2.positionX2 = 550;
+      walker2.positionX2 = 500;
+      walker2.positionX2 = 500;
     }
   }
   
